fcec503a1e376296fb6709d97e0e903a1b02447d
silvankasper <silvan.kasper@bluewin.ch>
2025-03-24 09:32:26 +0100

    Edit global design and login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,41 +4,25 @@ import { useRouter } from "next/navigation"; // use NextJS router for navigation
 import { useApi } from "@/hooks/useApi";
 import useLocalStorage from "@/hooks/useLocalStorage";
 import { User } from "@/types/user";
-import { Button, Form, Input } from "antd";
-// Optionally, you can import a CSS module or file for additional styling:
-// import styles from "@/styles/page.module.css";
-
-
-interface FormFieldProps {
-  label: string;
-  value: string;
-}
 
 const Login: React.FC = () => {
   const router = useRouter();
   const apiService = useApi();
-  const [form] = Form.useForm();
-  // useLocalStorage hook example use
-  // The hook returns an object with the value and two functions
-  // Simply choose what you need from the hook:
-  const {
-    // value: token, // is commented out because we do not need the token value
-    set: setToken, // we need this method to set the value of the token to the one we receive from the POST request to the backend server API
-    // clear: clearToken, // is commented out because we do not need to clear the token when logging in
-  } = useLocalStorage<string>("token", ""); // note that the key we are selecting is "token" and the default value we are setting is an empty string
-  // if you want to pick a different token, i.e "usertoken", the line above would look as follows: } = useLocalStorage<string>("usertoken", "");
+  const { set: setToken } = useLocalStorage<string>("token", "");
 
-  const handleLogin = async (values: FormFieldProps) => {
+  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const values = {
+      username: formData.get("username") as string,
+      password: formData.get("password") as string,
+    };
+
     try {
-      // Call the API service and let it handle JSON serialization and error handling
       const response = await apiService.post<User>("/users", values);
-
-      // Use the useLocalStorage hook that returned a setter function (setToken in line 41) to store the token if available
       if (response.token) {
         setToken(response.token);
       }
-
-      // Navigate to the user overview
       router.push("/users");
     } catch (error) {
       if (error instanceof Error) {
@@ -51,34 +35,32 @@ const Login: React.FC = () => {
 
   return (
     <div className="login-container">
-      <Form
-        form={form}
-        name="login"
-        size="large"
-        variant="outlined"
-        onFinish={handleLogin}
-        layout="vertical"
-      >
-        <Form.Item
+      <h1>Welcome to ScrabbleNow</h1>
+      <p>Who are you?</p>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
           name="username"
-          label="Username"
-          rules={[{ required: true, message: "Please input your username!" }]}
+          placeholder="username"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <button type="submit" className="login-button">
+          Login
+        </button>
+        <button
+          type="button"
+          className="login-button"
+          onClick={() => router.push("/register")}
         >
-          <Input placeholder="Enter username" />
-        </Form.Item>
-        <Form.Item
-          name="name"
-          label="Name"
-          rules={[{ required: true, message: "Please input your name!" }]}
-        >
-          <Input placeholder="Enter name" />
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-button">
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
+          Register
+        </button>
+      </form>
     </div>
   );
 };
